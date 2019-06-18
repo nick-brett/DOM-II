abd046b05467cebe66560166f633e3a8a201f2fc
Nicolas Brett <nicolasb59@gmail.com>
2019-06-18 17:54:36 -0400

    Added mouse over and click events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+
+/*create 10 unique event listeners to make Fun Bus 
+more interactive */
+
+//mouseover
+const funBus = document.querySelector('.logo-heading');
+funBus.addEventListener('mouseover', event => {
+    alert('All aboard the fun bus!');
+});
+//keydown
+const homeBtn = document.querySelectorAll('.nav-link');
+homeBtn.addEventListener('click', event => {
+    homeBtn.style.color = "red";
+});
+
+
+//wheel
+
+//drag / drop
+
+//load
+
+//focus
+
+//resize
+
+//scroll
+
+//select
+
+//dblclick
